3c300d2db729e9c0767ccca0efd64a7e58703d2d
Your Name <you@example.com>
2022-09-23 16:46:23 +0600

    fisrst

--- marketplace1/settings.py
+++ marketplace1/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static")
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
